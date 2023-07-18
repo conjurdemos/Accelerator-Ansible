87b006f67e0a9765c864c34378f8b82f41b35226
Joseph Hunt <jodyhunt@hotmail.com>
2023-07-18 12:46:57 -0500

    cover other CPU arch values in mysql build logic

--- demo/mysql/build/build.sh
+++ demo/mysql/build/build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 source ../../demo-vars.sh
 
-if [[ $(uname -p) == arm ]]; then
-  echo
-  echo "Building MySQL image for ARM architecture"
-  echo
-  $DOCKER build -t $MYSQL_IMAGE -f Dockerfile.arm .
-else
-  echo
-  echo "Building MySQL image for Intel architecture"
-  echo
-  $DOCKER build -t $MYSQL_IMAGE -f Dockerfile.i386 .
-fi
+ARCH=$(uname -p)
+
+case $ARCH in
+  arm | aarch64)
+    echo
+    echo "Building MySQL image for ARM architecture"
+    echo
+    $DOCKER build -t $MYSQL_IMAGE -f Dockerfile.arm .
+    ;;
+  i386 | x86_64)
+    echo
+    echo "Building MySQL image for Intel architecture"
+    echo
+    $DOCKER build -t $MYSQL_IMAGE -f Dockerfile.i386 .
+    ;;
+  *)
+    echo
+    echo "Unrecognized CPU architecture."
+esac
